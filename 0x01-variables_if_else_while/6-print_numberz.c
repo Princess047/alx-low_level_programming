fab5c81f48a71ce20286f57ef4e35250786b2381
Princess047 <princessemmanuel497@gmail.com>
2023-04-21 13:14:41 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(x + 48);
+	}
+	putchar('\n');
+	return (0);
+}
