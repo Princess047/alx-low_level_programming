4c7cc116018327050b1507ded5425031ae21db66
Princess047 <princessemmanuel497@gmail.com>
2023-04-25 07:40:50 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * numLength - returns the lenth of string
+ * @num : operand number
+ * Return: number of digits
+ */
+
+int numLength(int num)
+{
+	int length = 0;
+
+	if (!num)
+	{
+		return (1);
+	}
+}
